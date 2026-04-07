cdaa82b99a127b3cde3a53d8f60abd13a7d78c7e
EP-Probability <joshuahart2064@gmail.com>
2026-04-07 09:32:14 +0200

    Update AssignmentHomePage.py

--- AssignmentHomePage.py
+++ AssignmentHomePage.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title('Welcome to the homepage of the MAM2041F Assignment by HRTJOS011')
 
-st.write("In this assignment we will be taking a look at in part one, the lagrange interpolating polynomial as well as Divided differences and Newton's form. Then in part two we take a look at the bisection method and secant method view their results and comapre them.")
+st.write("In this assignment we will be taking a look at in part one, the Lagrange interpolating polynomial as well as Divided differences and Newton's form. Then in part two we take a look at the bisection method and secant method view their results and comapre them.")
 st.write("I did not use AI at all for this assignment as I believe the use of AI not made by you and not hosted on your hardware is unethical.")
 
 col1,col2,col3 = st.columns(3)
 
 with col1:
-    if st.button('press here to begin part 1 question 1. Lagrange Interpoation'):
+    if st.button('press here to begin part 1 question 1. Lagrange Interpolation'):
         st.switch_page('pages/Lagrange.py')
 
 with col2:
@@ -16,6 +16,6 @@ with col2:
         st.switch_page('pages/DividedDifferences.py')
 
 with col3:
-    if st.button("Press here to being part 2. Root Finding"):
+    if st.button("Press here to begin part 2. Root Finding"):
         st.switch_page('pages/RootFinding.py')
 
